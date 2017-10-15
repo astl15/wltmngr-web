b7decd6a24a9c9dc4d29964580d05b7223314c65
Andrei Stelea <stelea.andrei@gmail.com>
2017-10-16 00:23:49 +0300

    Add log4j2

--- src/ro/astl/wltmngr/controller/MainController.java
+++ src/ro/astl/wltmngr/controller/MainController.java
@@ -1,13 +1,17 @@
 package ro.astl.wltmngr.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-	
+	private static final Logger logger = (Logger) LogManager.getLogger(MainController.class);
+
 	@RequestMapping("/")
 	public String home(){
-		return "register";
+		logger.debug("/ Request Mapped");
+		return "home";
 	}
 }
