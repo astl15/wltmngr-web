d2df41e8b85bcce3a4449acebc8da60274fd667b
Andrei Stelea <stelea.andrei@gmail.com>
2017-12-03 13:46:40 +0200

    Add missing file from previous commit

---
+++ src/ro/astl/wltmngr/model/CategoryAmount.java
@@ -0,0 +1,21 @@
+package ro.astl.wltmngr.model;
+
+public class CategoryAmount {
+	
+	private String category;
+	private float amount;
+	
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+
+}
